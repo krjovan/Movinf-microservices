05a141fcb2b12e4d599e5a1e8602358a25f043b4
krjovan <jovankron@gmail.com>
2021-08-16 18:39:15 +0200

    Added Swagger documentation

--- api/src/main/java/com/example/api/composite/movie/MovieCompositeService.java
+++ api/src/main/java/com/example/api/composite/movie/MovieCompositeService.java
@@ -3,6 +3,13 @@ package com.example.api.composite.movie;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+
+@Api(description = "REST API for composite movie information.")
 public interface MovieCompositeService {
 
     /**
@@ -11,6 +18,14 @@ public interface MovieCompositeService {
      * @param movieId
      * @return the composite movie info, if found, else null
      */
+	@ApiOperation(
+	        value = "${api.movie-composite.get-composite-movie.description}",
+	        notes = "${api.movie-composite.get-composite-movie.notes}")
+    @ApiResponses(value = {
+        @ApiResponse(code = 400, message = "Bad Request, invalid format of the request. See response message for more information."),
+        @ApiResponse(code = 404, message = "Not found, the specified id does not exist."),
+        @ApiResponse(code = 422, message = "Unprocessable entity, input parameters caused the processing to fails. See response message for more information.")
+    })
     @GetMapping(
         value    = "/movie-composite/{movieId}",
         produces = "application/json")
